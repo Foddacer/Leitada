5825ab4d8e92842b210d64240d3a0567acea5034
Foddacer <146361808+Foddacer@users.noreply.github.com>
2023-10-26 12:19:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,56 +1,17 @@
- function tocaSomPom (){
+
+function tocaSomPom (){
     document.querySelector('#som_tecla_pom').play();
  }
 
-document.querySelector('tecla_pom').onclick = tocaSomPom();
+const ListaDeTeclas = document.querySelectorAll('.tecla');
 
+ListaDeTeclas[0].onclick = tocaSomPom;
 
-function tocaSomClap (){
-    document.querySelector('#som_tecla_clap').play();
- }
-
-document.querySelector('tecla_clap').onclick = tocaSomClap();
-
-
-function tocaSomPuff (){
-    document.querySelector('#som_tecla_puff').play();
- }
-
-document.querySelector('tecla_puff').onclick = tocaSomPuff();
-
-
-function tocaSomSplash (){
-    document.querySelector('#som_tecla_splash').play();
- }
-
-document.querySelector('tecla_splash').onclick = tocaSomSplash();
-
-
-function tocaSomToim (){
-    document.querySelector('#som_tecla_toim').play();
- }
-
-document.querySelector('tecla_toim').onclick = tocaSomToim();
-
-
-function tocaSomPsh (){
-    document.querySelector('#som_tecla_psh').play();
- }
-
-document.querySelector('tecla_psh').onclick = tocaSomPsh();
-
-
-function tocaSomTic (){
-    document.querySelector('#som_tecla_tic').play();
- }
-
-document.querySelector('tecla_tic').onclick = tocaSomTic();
-
-
-function tocaSomTom (){
-    document.querySelector('#som_tecla_tom').play();
- }
-
-document.querySelector('tecla_tom').onclick = tocaSomTom();
-
-
+ListaDeTeclas[1].onclick = tocaSomClap;
+ListaDeTeclas[2].onclick = tocaSomTim;
+ListaDeTeclas[3].onclick = tocaSomPuff;
+ListaDeTeclas[4].onclick = tocaSomSplash;
+ListaDeTeclas[5].onclick = tocaSomToim;
+ListaDeTeclas[6].onclick = tocaSomPsh;
+ListaDeTeclas[7].onclick = tocaSomTic;
+ListaDeTeclas[8].onclick = tocaSomTom;
